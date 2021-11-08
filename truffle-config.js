81207c39120a5f555f031e783a2b2052e1be48fe
seiriosalpha <seiriosalpha@gmail.com>
2021-11-08 17:45:41 +0800

    Update Contract

---
+++ truffle-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  networks: {
+    local: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*', // Match any network id
+    },
+  },
+  compilers: {
+    solc: {
+      version: '^0.8.3',
+    },
+  },
+}
